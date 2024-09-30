7a816541d350bebcfc5212fb10abc80ca1b81f14
Colton Hicks <github@coltonhicks.com>
2024-09-30 15:37:59 -0700

    Initial cli hacking to get basic commands working.

--- qcop/cli.py
+++ qcop/cli.py
@@ -1,50 +1,139 @@
+import json
+import tempfile
 from pathlib import Path
-from typing import Any, Optional
+from typing import Annotated, Any, Optional
 
 import toml
 import typer
-from pydantic import BaseModel
-from qcio import ProgramInput
+import yaml
+from pydantic import ValidationError
+from qcio import DualProgramInput, ProgramInput, Structure, utils
+from rich import print as rprint
+
+from qcop import compute as qcop_compute
 
 app = typer.Typer()
 
 
-# NOTE: This is not working. Just an initial idea from ChatGPT.
-def create_empty_dict_from_model(model_class: Any) -> dict:
-    empty_model = {}
-    for field_name, field in model_class.model_fields.items():
-        field_type = field.annotation.__class__
-        if issubclass(field_type, BaseModel):
-            # If it's a nested model, recursively process it
-            empty_model[field_name] = create_empty_dict_from_model(field_type)
-        else:
-            # Set to None, an empty string, or a suitable default
-            empty_model[field_name] = ""
-    return empty_model
+def _serialize_data(data: Any, filename: Path):
+    kwargs = {}
+    if filename.suffix == ".toml":
+        dump = toml.dump
+    elif filename.suffix == ".yaml" or filename.suffix == ".yml":
+        dump = yaml.dump
+    elif filename.suffix == ".json":
+        dump = json.dump
+        kwargs = {"indent": 2}
+    else:
+        raise ValueError(f"Unsupported file extension: {filename.suffix}")
+    with open(filename, "w") as f:
+        dump(data, f, **kwargs)
+
+
+def _deserialize_data(filename: Path) -> Any:
+    if filename.suffix == ".toml":
+        load = toml.load
+    elif filename.suffix == ".yaml" or filename.suffix == ".yml":
+        load = yaml.load
+    elif filename.suffix == ".json":
+        load = json.load
+    else:
+        raise ValueError(f"Unsupported file extension: {filename.suffix}")
+    with open(filename, "r") as f:
+        return load(f)
 
 
 @app.command()
-def new(filename: Optional[Path] = "input.toml", dual_program: bool = False):
+def new(
+    filename: Path,
+    dual_program: Annotated[
+        bool, typer.Option("--dual-program", "-dp", help="Create a DualProgramInput")
+    ] = False,
+    calctype: Annotated[
+        str, typer.Option("--calctype", "-c", help="calctype for the input file")
+    ] = "energy",
+    no_structure: Annotated[
+        bool,
+        typer.Option(
+            "--no-structure", "-ns", help="Exclude the structure from the input file"
+        ),
+    ] = False,
+):
     """Create a new program input file."""
-    count = 1
-    while filename.exists():
-        typer.echo(f"File already exists: {filename}")
-        filename = f"{filename.stem}_{count}{filename.suffix}"
-        count += 1
 
-    typer.echo(f"Creating a new input file: {filename}")
-    model_dict = create_empty_dict_from_model(ProgramInput)
-    # Convert to TOML and write to file
-    import pdb
+    rprint(f"Creating a new input file: {filename}")
+    if not dual_program:
+        prog_inp = ProgramInput(
+            calctype=calctype,
+            structure=utils.water,
+            model={"method": "b3lyp", "basis": "6-31g"},
+            keywords={"key1": "value1"},
+        )
+    else:
+        prog_inp = DualProgramInput(
+            calctype=calctype,
+            structure=utils.water,
+            subprogram="subprogram_name",
+            keywords={"key1": "value1"},
+            subprogram_args={
+                "model": {"method": "b3lyp", "basis": "6-31g"},
+                "keywords": {"key1": "value1"},
+            },
+        )
 
-    pdb.set_trace()
-    with open("empty_model.toml", "w") as file:
-        toml.dump(model_dict, file)
+    # Serialize data
+    serialized_data = prog_inp.model_dump(exclude_unset=True, exclude_none=True)
+    serialized_data["structure"].pop("connectivity")
+    if no_structure:
+        serialized_data.pop("structure")
+
+    _serialize_data(serialized_data, filename)
 
 
 @app.command()
-def two():
-    pass
+def compute(
+    input_filename: Path,
+    output_filename: Annotated[
+        Path,
+        typer.Option("--output", "-o", help="Output filename"),
+    ],
+    program: Annotated[
+        str, typer.Option("--program", "-p", help="Program name for the input file")
+    ],
+    structure: Optional[Path] = None,
+):
+    """Compute the input file and save the results to the output_filename."""
+    rprint(f"Computing {input_filename} and saving to {output_filename}")
+
+    if structure is not None:
+        rprint(f"Using the structure file: {structure}")
+        struct = Structure.open(structure)
+        prog_inp_dict = _deserialize_data(input_filename)
+        prog_inp_dict["structure"] = struct.model_dump()
+        input_filename = Path(
+            tempfile.NamedTemporaryFile(delete=True, suffix=".toml").name
+        )
+        _serialize_data(prog_inp_dict, input_filename)
+
+    try:
+        prog_inp = ProgramInput.open(input_filename)
+    except ValidationError:
+        try:
+            prog_inp = DualProgramInput.open(input_filename)
+        except ValidationError:
+            rprint(f"Invalid input file: {input_filename}")
+            raise typer.Exit(1)
+
+    prog_output = qcop_compute(program, prog_inp, print_stdout=True, raise_exc=False)
+
+    if prog_output.success:
+        rprint("[bold green]Computation successful![/bold green]")
+    else:
+        rprint(prog_output.traceback)
+        rprint("[bold red]Computation failed![/bold red]")
+
+    rprint(f"Saving the results to {output_filename}")
+    _serialize_data(prog_output.model_dump(), output_filename)
 
 
 if __name__ == "__main__":
