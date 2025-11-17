d3af1a838bc6c7566ec4e2bc3d9b369583c3480b
avcopan <avcopan@gmail.com>
2025-11-17 14:50:11 -0500

    Add Orca adapter
    The only quirk relative to the existing TeraChem adapter is that, when
    running a parallelized job, Orca requires one to use the full executable
    path:
    ```
    /full/path/to/orca orca.inp
    ```
    Instead of simply doing `orca orca.inp`.

--- src/qcop/adapters/__init__.py
+++ src/qcop/adapters/__init__.py
@@ -4,6 +4,7 @@ from .base import *  # noqa: F403
 from .crest import CRESTAdapter  # noqa: F401
 from .file import FileAdapter  # noqa: F401
 from .geometric import GeometricAdapter  # noqa: F401
+from .orca import OrcaAdapter  # noqa: F401
 from .qcengine import QCEngineAdapter  # noqa: F401
 from .terachem import TeraChemAdapter  # noqa: F401
 from .terachem_fe import TeraChemFEAdapter  # noqa: F401
